609a2bf768c3e3a151b9d25c6e1c003803b14baf
derek <derek.crew@scritchlabs.com>
2024-11-11 23:20:46 -0700

    Added github action variables

--- github.tf
+++ github.tf
@@ -1,6 +1,22 @@
 //  Create the github repository
 resource "github_repository" "repo" {
-  name        = var.github_repo_name
-  visibility = "private"
+  name                 = var.github_repo_name
+  visibility           = "private"
   vulnerability_alerts = false
+}
+
+resource "github_actions_variable" "s3_url_variable" {
+  repository    = github_repository.repo.name
+  variable_name = "S3_URL"
+  value         = aws_s3_bucket.my_bucket.bucket
+
+  depends_on = [aws_s3_bucket.my_bucket, github_repository.repo]
+}
+
+resource "github_actions_variable" "iam_role_arn_variable" {
+  repository    = github_repository.repo.name
+  variable_name = "IAM_DEPLOYMENT_ROLE_ARN"
+  value         = aws_iam_role.role_to_access.arn
+
+  depends_on = [aws_iam_role.role_to_access, github_repository.repo]
 }
