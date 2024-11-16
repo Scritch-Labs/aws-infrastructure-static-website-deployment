84b112a47d92f548031b287979304d858f0780e2
derek <derek.crew@scritchlabs.com>
2024-11-16 11:18:25 -0700

    Renamed S3 env variable

--- github.tf
+++ github.tf
@@ -7,7 +7,7 @@ resource "github_repository" "repo" {
 
 resource "github_actions_variable" "s3_url_variable" {
   repository    = github_repository.repo.name
-  variable_name = "S3_URL"
+  variable_name = "S3_BUCKET_NAME"
   value         = aws_s3_bucket.my_bucket.bucket
 
   depends_on = [aws_s3_bucket.my_bucket, github_repository.repo]
