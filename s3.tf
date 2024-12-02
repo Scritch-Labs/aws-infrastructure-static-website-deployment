1c618436a85a012f2f4fd025135255dc0d58e743
derek <derek.crew@scritchlabs.com>
2024-12-02 00:44:06 -0700

    Added s3:DeleteObject

--- s3.tf
+++ s3.tf
@@ -13,7 +13,7 @@ resource "aws_iam_policy" "s3_access_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = ["s3:ListBucket", "s3:GetObject", "s3:PutObject"],
+        Action = ["s3:ListBucket", "s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
         Effect = "Allow",
         Resource = [
           aws_s3_bucket.my_bucket.arn,
